ed6eefa82d06b88d5a6d8068a035a6f9ba206526
Dmitrii <96414168+DMITRII1548@users.noreply.github.com>
2025-10-27 18:49:19 +0600

    Install and configure rector

---
+++ rector.php
@@ -0,0 +1,25 @@
+<?php
+
+use Rector\Config\RectorConfig;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/src', 
+        __DIR__ . '/tests'
+    ])
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        typeDeclarations: true,
+        privatization: false,
+        naming: true,
+        instanceOf: true,
+        earlyReturn: true,
+        strictBooleans: false,
+        phpunitCodeQuality: true,
+        doctrineCodeQuality: true,
+        symfonyCodeQuality: true,
+        symfonyConfigs: true
+    )
+    ->withPhpSets(php84: true)
+    ->withParallel();
